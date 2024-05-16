4ed9f4fbd14097246898df4439f35b5707135a7a
Sagar2k1 <84672270+Sagar2k1@users.noreply.github.com>
2024-05-16 21:03:10 +0700

    Lâm first commit

---
+++ split_frame.py
@@ -0,0 +1,23 @@
+from utils import read_video
+import cv2
+import os
+import time
+
+path = 'data\Video'
+path_saved = 'Frame'
+step = 20
+
+for i in os.listdir(path):
+    file_vid = os.path.join(path, i)
+    video_name = i.split('.')[0]
+    location_saved = os.path.join(path_saved, video_name)
+    if not os.path.exists(location_saved):
+        os.mkdir(location_saved, 0o666)
+    frames = read_video(file_vid, step)
+    count = 0
+    for frame in frames:
+        name = f'{video_name}_{count}.jpg'
+        cv2.imwrite(os.path.join(location_saved,name), frame)
+        print(f'created {name} in {location_saved}')
+        count +=1
+        time.sleep(1)
---
+++ utils.py
@@ -0,0 +1,23 @@
+import cv2
+import time
+
+def read_video(file, step, duration = None):
+    frame_list = []
+    count = 0
+    vid = cv2.VideoCapture(file)
+    while True:
+        _, frame = vid.read()
+        count +=1
+        if _== False:
+            break
+        elif count % step == 2:
+            # h, w, _ = frame.shape
+            # frame = cv2.resize(frame,(int(w/2),int(h/2)))
+            frame_list.append(frame)
+        if duration == None:
+            pass
+        elif count == duration:
+            break
+    return frame_list
+
+
